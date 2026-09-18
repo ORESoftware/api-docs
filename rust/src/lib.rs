daa369a3e539c8e20f839349907ca0638e2ba680
Alexander Mills <alex@oresoftware.com>
2026-09-17 22:34:21 -0500

    DEN-4078 apply rustfmt to stream module exports

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -13,11 +13,11 @@
 
 pub mod binding;
 pub mod call;
-pub mod client_stream;
 pub mod catalog;
 pub mod client_codegen;
 pub mod client_codegen_v2;
 pub mod client_codegen_v3;
+pub mod client_stream;
 pub mod discovery;
 pub mod fs_codegen;
 pub mod fs_discovery;
@@ -68,11 +68,6 @@ pub mod rpc_shared_operation;
 pub mod typed_operation_context;
 
 pub use binding::{RouteBinding, RpcHttp, RpcMethod, RpcTransport, UnaryFn};
-pub use client_stream::{
-    FramedRpcStream, OresRpcStreamClient, RpcStreamCall, RpcStreamCallBuilder, RpcStreamCarrier,
-    RpcStreamClient, RpcStreamContext, RpcStreamError, RpcStreamFrame, RpcStreamPrepareError,
-    RpcStreamRequest, RpcStreamSession,
-};
 pub use call::{
     encode_length_prefixed, split_length_prefixed, RpcCall, RpcReceipt, Transport, MAX_FRAME_BYTES,
 };
@@ -82,6 +77,11 @@ pub use client_codegen_v2::{rpc_client_bundle_v2, RpcClientBundleV2, RpcClientBu
 pub use client_codegen_v3::{
     rpc_client_bundle_v3, RpcClientBundleV3, RpcClientBundleV3Manifest,
     RpcClientTransportSourcesV3, RpcOperationClientSourcesV3,
+};
+pub use client_stream::{
+    FramedRpcStream, OresRpcStreamClient, RpcStreamCall, RpcStreamCallBuilder, RpcStreamCarrier,
+    RpcStreamClient, RpcStreamContext, RpcStreamError, RpcStreamFrame, RpcStreamPrepareError,
+    RpcStreamRequest, RpcStreamSession,
 };
 pub use discovery::{DocsDiscoveryManifest, DocsProjectionRoutes, DISCOVERY_SCHEMA_VERSION};
 pub use fs_codegen::{api_compile_glue, api_server_glue, page_compile_glue};
