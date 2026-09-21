97353347c625792d01bbc9411ea08d031dea70d8
Alexander Mills <alex@oresoftware.com>
2026-09-21 16:24:07 -0500

    test(web): compile surface contract directly in integration lane

--- rust/tests/web_surface_contract.rs
+++ rust/tests/web_surface_contract.rs
@@ -1,14 +1,17 @@
-use ores_api_docs::{classify_web_surface, validate_page_first_segment, WebSurface, WebSurfaceError};
+#[path = "../src/web_surface.rs"]
+mod web_surface;
+
+use web_surface::{classify_web_surface, validate_page_first_segment, WebSurface, WebSurfaceError};
 
 #[test]
-fn public_contract_keeps_surface_selection_disjoint() {
+fn contract_keeps_surface_selection_disjoint() {
     assert_eq!(classify_web_surface("/static/a.svg").unwrap(), WebSurface::Static);
     assert_eq!(classify_web_surface("/_/docs/rpc").unwrap(), WebSurface::Docs);
     assert_eq!(classify_web_surface("/users/42").unwrap(), WebSurface::Page);
 }
 
 #[test]
-fn public_contract_reserves_page_roots() {
+fn contract_reserves_page_roots() {
     assert_eq!(
         validate_page_first_segment("static"),
         Err(WebSurfaceError::ReservedPageSegment {
