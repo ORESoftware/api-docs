61608449e39f5140a608827202a079a3f9079055
Alexander Mills <alex@oresoftware.com>
2026-09-21 16:27:23 -0500

    test(web): cover typed GET HEAD classifier

--- rust/tests/web_surface_contract.rs
+++ rust/tests/web_surface_contract.rs
@@ -1,17 +1,29 @@
 #[path = "../src/web_surface.rs"]
 mod web_surface;
 
-use web_surface::{classify_web_surface, validate_page_first_segment, WebSurface, WebSurfaceError};
+use web_surface::{
+    classify_web_request, validate_page_first_segment, WebRequestMethod, WebSurface,
+    WebSurfaceError,
+};
 
 #[test]
 fn contract_keeps_surface_selection_disjoint() {
-    assert_eq!(classify_web_surface("/static/a.svg").unwrap(), WebSurface::Static);
-    assert_eq!(classify_web_surface("/_/docs/rpc").unwrap(), WebSurface::Docs);
-    assert_eq!(classify_web_surface("/users/42").unwrap(), WebSurface::Page);
+    assert_eq!(
+        classify_web_request(WebRequestMethod::Get, "/static/a.svg").unwrap(),
+        WebSurface::Static
+    );
+    assert_eq!(
+        classify_web_request(WebRequestMethod::Head, "/_/docs/rpc").unwrap(),
+        WebSurface::Docs
+    );
+    assert_eq!(
+        classify_web_request(WebRequestMethod::Get, "/users/42").unwrap(),
+        WebSurface::Page
+    );
 }
 
 #[test]
-fn contract_reserves_page_roots() {
+fn contract_reserves_page_roots_and_internal_namespace() {
     assert_eq!(
         validate_page_first_segment("static"),
         Err(WebSurfaceError::ReservedPageSegment {
@@ -19,4 +31,10 @@ fn contract_reserves_page_roots() {
         })
     );
     assert!(validate_page_first_segment("users").is_ok());
+    assert_eq!(
+        classify_web_request(WebRequestMethod::Get, "/_/unknown"),
+        Err(WebSurfaceError::ReservedInternalPath {
+            path: "/_/unknown".to_owned(),
+        })
+    );
 }
